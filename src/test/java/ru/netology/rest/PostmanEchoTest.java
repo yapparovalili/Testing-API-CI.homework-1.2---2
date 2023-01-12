3eab155ec92a4bf98f165bc00cb320a65f22030e
Liliya Yapparova <yapparovalili@yandex.ru>
2023-01-12 22:28:09 +0700

    Changed test

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -3,9 +3,6 @@ package ru.netology.rest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-
 import static org.hamcrest.Matchers.equalTo;
 
 
@@ -15,12 +12,12 @@ class PostmanEchoTest {
     void shouldReturnSendData() {
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data")
+                .body("some value")
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("date", equalTo("Data"))
-                .body(matchesJsonSchemaInClasspath("postmanecho.schema.json"));
+                .body("data", equalTo("some data"));
+
     }
 }
